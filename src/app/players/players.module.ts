53fb095732054f999ecd372c1c662fba6b67ff46
Anna Moreta <anuskam92@gmail.com>
2024-06-22 23:14:58 +0200

    add abstract class to service

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// i18n
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslation } from '../core/config/i18n/translate-loader.config';
+
 import { PlayersRoutingModule } from './players-routing.module';
 import { PlayersListComponent } from './players-list/players-list.component';
 import { PlayersCardComponent } from './players-card/players-card.component';
 import { PlayersVideoComponent } from './players-video/players-video.component';
 import { SafeUrlPipe } from '../utils/pipes/safe-url.pipe';
 import { PlayersService } from '../services/players.service';
+import { PlayersPublicService } from '../services/players-public.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +22,15 @@ import { PlayersService } from '../services/players.service';
     PlayersVideoComponent,
     SafeUrlPipe,
   ],
-  imports: [CommonModule, PlayersRoutingModule],
-  providers: [PlayersService],
+  imports: [
+    CommonModule,
+    PlayersRoutingModule,
+    TranslateModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: PlayersService, useClass: PlayersPublicService },
+    importProvidersFrom(TranslateModule.forRoot(provideTranslation())),
+  ],
 })
 export class PlayersModule {}
--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -1,43 +1,12 @@
-import { Injectable, inject } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 import { IPlayer } from '../interfaces/IPlayer.interface';
-import { IPlayersService } from '../interfaces/players-service.interface';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class PlayersService implements IPlayersService {
-  private playersUrl = environment.apiUrl;
-  private http = inject(HttpClient);
-
-  getPlayers(): Observable<IPlayer[]> {
-    return this.http.get<{ players: IPlayer[] }>(this.playersUrl).pipe(
-      map(response => response.players),
-      catchError(this.handleError),
-    );
-  }
-
-  getPlayerById(id: number): Observable<IPlayer | undefined> {
-    return this.getPlayers().pipe(
-      map((players: IPlayer[]) => players.find(player => player.id === id)),
-      catchError(this.handleError),
-    );
-  }
-
-  getPlayerVideos(id: number): Observable<string[]> {
-    return this.getPlayerById(id).pipe(
-      map(player => (player ? player.videos : [])),
-      catchError(this.handleError),
-    );
-  }
-
-  private handleError(error: HttpErrorResponse): Observable<never> {
-    console.error('An error occurred:', error.message);
-    return throwError(
-      () => new Error('Something bad happened; please try again later.'),
-    );
-  }
+@Injectable()
+export abstract class PlayersService {
+  abstract getPlayers(): Observable<IPlayer[]>;
+  abstract getPlayerById(id: number): Observable<IPlayer | undefined>;
+  abstract getPlayerVideos(id: number): Observable<string[]>;
+  abstract handleError(error: HttpErrorResponse): Observable<never>;
 }
