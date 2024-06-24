b90dd606f908a0a8f41d782b4acd2de7d4bbc294
Anna Moreta <anuskam92@gmail.com>
2024-06-25 00:06:41 +0200

    add all the services and modules to playerscard to run first test

--- src/app/players/players-card/players-card.component.spec.ts
+++ src/app/players/players-card/players-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PlayersCardComponent } from './players-card.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlayersService } from '../../services/players.service';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 
 describe('PlayersCardComponent', () => {
   let component: PlayersCardComponent;
@@ -9,6 +12,12 @@ describe('PlayersCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PlayersCardComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        RouterTestingModule.withRoutes([]),
+        BreadcrumbModule,
+      ],
+      providers: [TranslateService, PlayersService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PlayersCardComponent);
