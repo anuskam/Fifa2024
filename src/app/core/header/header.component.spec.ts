e47e444886de86fe844d381531e7c1ccef3f3388
Anna Moreta <anuskam92@gmail.com>
2024-06-24 23:57:13 +0200

    add test for header and add configuration for translated words

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,6 +10,8 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
+      imports: [TranslateModule.forRoot()],
+      providers: [TranslateService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -18,5 +21,30 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the FIFA logo', () => {
+    const compiled = fixture.nativeElement;
+    const imgElement = compiled.querySelector('.header__logo img');
+    expect(imgElement).toBeTruthy();
+    expect(imgElement.src).toContain('fifa-logo.png');
+  });
+
+  it('should render language buttons', () => {
+    const compiled = fixture.nativeElement;
+    const buttons = compiled.querySelectorAll('.header__multilanguage-button');
+    expect(buttons.length).toBe(component.languages.length);
+  });
+
+  it('should call getLanguage method when a language button is clicked', () => {
+    spyOn(component, 'getLanguage');
+    const button = fixture.debugElement.query(
+      By.css('.header__multilanguage-button'),
+    );
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.getLanguage).toHaveBeenCalledWith(
+      component.languages[0].value,
+    );
   });
 });
